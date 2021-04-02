758f69537f2118280328684b075cf2c876410cc4
Dark Mode : On <muffintoppings36@gmail.com>
2021-04-02 21:41:13 +0800

    Initial commit

---
+++ src/main/java/com/darkmode/sheeper/SheeperApplication.java
@@ -0,0 +1,13 @@
+package com.darkmode.sheeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SheeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SheeperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/darkmode/sheeper/SheeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darkmode.sheeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SheeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
